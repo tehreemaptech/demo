36fe5984e6cdacea54d6459caceacf1aae887aa8
tehreemaptech <tehreemmushtaqaptechnn@gmail.com>
2022-06-23 12:19:57 +0500

    first.php modified and added a readme.txt file

--- first.php
+++ first.php
@@ -3,5 +3,8 @@
 
 echo "<h2>Hello Batch 2011C3</h2>";
 
+include("second.php");
+
+echo "<p>this is the second line of file first.php</p>";
 
 ?>
---
+++ readme.txt
@@ -0,0 +1,3 @@
+this is demo read me file
+
+2nd line of readme.txt
